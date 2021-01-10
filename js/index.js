47ed1361d3dea1c7b07dfc05d525ae80ed1762c4
Prajjwal Singh <prajjwal.iiitdmj@gmail.com>
2021-01-10 14:09:28 +0530

    Multiple color changes and localStorage functionality to persist the data

--- js/index.js
+++ js/index.js
@@ -3,6 +3,28 @@ const addListBtn = document.getElementById("add-list-button");
 
 addListBtn.addEventListener("click", showAddListModal);
 
+window.onload = function () {
+    const initalData = [{
+        title: "Teams",
+        id: getId(),
+        cardColor: getCardColor(),
+        cards: [{ id: getId(), cardTitle: "Product", cardDesc: "3 pending tasks to be picked by Raj" }, { id: getId(), cardTitle: "Sales", cardDesc: "Send proposals to Punnet for sales prices." }]
+    }, {
+        title: "Products",
+        id: getId(),
+        cardColor: getCardColor(),
+        cards: [{ id: getId(), cardTitle: "UAT Testing", cardDesc: "Ask engg. to setup testing infrastructure" }]
+    }]
+    console.log("I had came here")
+    let lists = window.localStorage.getItem('lists');
+
+    if (!lists) {
+        window.localStorage.setItem("lists", JSON.stringify(initalData));
+        lists = window.localStorage
+    }
+    createInitalListsAndCards(JSON.parse(lists));
+};
+
 window.addEventListener("keyup", function (event) {
     const addListModal = document.getElementById("add-list-modal");
     const addCardModal = document.getElementById("add-card-modal");
@@ -27,6 +49,7 @@ function showAddListModal() {
     createListBtn.addEventListener("click", function (event) {
         event.preventDefault();
         const newTitle = document.getElementById("list-name-input").value;
+        insertList(newTitle);
         createList(newTitle);
         addListModal.remove();
     });
@@ -59,16 +82,15 @@ function createList(title) {
         event.preventDefault();
         this.parentNode.parentNode.remove();
     });
-    const listBody = document.getElementById(`list-body-${title}`);
     const addCardBtn = document.getElementById(`add-card-${title}`);
     addCardBtn.addEventListener("click", function (event) {
         event.preventDefault();
-        showAddCardModal.call(this, listBody);
+        showAddCardModal.call(this, title);
     });
 }
 
 // function to show the Modal to add Add Card
-function showAddCardModal(parentNode) {
+function showAddCardModal(listTitle) {
     const addCardModal = document.createElement("div");
     addCardModal.className = "modal-wrapper";
     addCardModal.id = "add-card-modal";
@@ -80,7 +102,9 @@ function showAddCardModal(parentNode) {
         event.preventDefault();
         const newCardTitle = document.getElementById("card-title-input").value;
         const newCardDesc = document.getElementById("card-desc-text-area").value;
-        createCard(parentNode, newCardTitle, newCardDesc);
+        insertCard(listTitle, newCardTitle, newCardDesc);
+        const cardColor = findList(listTitle).cardColor;
+        createCard(listTitle, newCardTitle, newCardDesc, cardColor);
         addCardModal.remove();
     });
     const cardDescTxtArea = document.getElementById("card-desc-text-area");
@@ -97,16 +121,17 @@ function showAddCardModal(parentNode) {
 }
 
 // function to create a card
-function createCard(parentNode, cardTitle, cardDesc) {
-    const color = getCardColor();
+function createCard(listTitle, cardTitle, cardDesc, cardColor) {
+    console.log("cardColor - ", cardColor);
     const newCard = document.createElement("div");
     newCard.className = "list-card";
     newCard.id = `${cardTitle}-card`;
-    newCard.style.backgroundColor = color;
+    newCard.style.backgroundColor = cardColor;
     newCard.setAttribute("draggable", true);
     newCard.setAttribute("ondragstart", "drag(event)");
     const innerHtml = `<div class="card-header"><div class="card-header-content">${cardTitle}</div><img class="card-close" id="card-close-${cardTitle}" height="40%" src="./assets/close.svg" /></div><div class="card-body">${cardDesc}</div>`;
     newCard.innerHTML = innerHtml;
+    const parentNode = document.getElementById(`list-body-${listTitle}`);
     parentNode.appendChild(newCard);
     const closeIcon = document.getElementById(`card-close-${cardTitle}`);
     closeIcon.addEventListener("click", function (event) {
@@ -134,4 +159,61 @@ function getCardColor() {
     const colorArr = ["lightsalmon", "cornflowerblue", "greenyellow", "lightpink", "palevioletred", "#FED000", "violet", "orange"];
     const index = Math.floor(Math.random() * Math.floor(colorArr.length - 1));
     return colorArr[index];
+}
+
+function getId() {
+    var S4 = function () {
+        return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    };
+    return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+}
+
+function createInitalListsAndCards(lists) {
+    for (let i = 0; i < lists.length; i++) {
+        const listTitle = lists[i].title;
+        const cardColor = lists[i].cardColor;
+        createList(listTitle);
+        for (let j = 0; j < lists[i].cards.length; j++) {
+            const currCard = lists[i].cards[j];
+            createCard(listTitle, currCard.cardTitle, currCard.cardDesc, cardColor);
+        }
+    }
+}
+
+function findList(id){
+    const reqList = JSON.parse(localStorage.getItem("lists")).filter(list=> list.title === id)[0];
+    return reqList;
+}
+
+function findCard(listId, cardId){
+    const reqCard = findList(listId).cards.filter(card => card.id === cardId);
+    return reqCard;
+}
+
+function insertList(newTitle) {
+    const currLists = JSON.parse(localStorage.getItem("lists"));
+    const newList = {
+        id: getId(),
+        title: newTitle,
+        cardColor: getCardColor(),
+        cards: []
+    }
+    currLists.push(newList);
+    localStorage.setItem("lists", JSON.stringify(currLists));
+}
+
+function insertCard(listId, cardTitle, cardDesc){
+    const currLists = JSON.parse(localStorage.getItem("lists"));
+    const newCard = {
+        id: getId(),
+        cardTitle,
+        cardDesc
+    }
+    currLists.forEach(list => {
+        // change list.title to list.id later when id logic is implemented
+        if(list.title === listId){
+            list.cards.push(newCard);
+        }
+    });
+    localStorage.setItem("lists", JSON.stringify(currLists));
 }
